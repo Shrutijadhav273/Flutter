04fb34e34ff92765a6d6a6cfa5d112d794178b59
Shrutijadhav273 <shrutijadhav969@gmail.com>
2026-01-23 20:16:02 +0530

    palindrome

--- Dart-basic/palindrome.dart
+++ Dart-basic/palindrome.dart
@@ -1,5 +1,22 @@
 import 'dart:io';
 
-void main(){
-    
+void main()
+{
+    stdout.write("enter number");
+    int num=int.parse(stdin.readLineSync()!);
+    int rev=0;
+    int org=num;
+
+    while(num!=0)
+    {
+        int digit=num%10;
+        rev=rev*10+digit;
+        num=num~/10;
+    }
+    if(org==rev){
+        print("yes");
+    }
+    else{
+        print("no");
+    }
 }
