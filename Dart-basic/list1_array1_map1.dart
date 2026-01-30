285717748a87420480f7ef84927d71da9e90b320
Shrutijadhav273 <shrutijadhav969@gmail.com>
2026-01-30 21:55:37 +0530

    changes

---
+++ Dart-basic/list1_array1_map1.dart
@@ -0,0 +1,21 @@
+/*void main(){
+    List<int> no=[2,2,3,4,5,6];
+    //vertical order printing 
+    for (var i in no){
+        print(i);
+    //print(no[2]);
+    }
+    
+   
+}
+
+*/
+
+
+void main(){
+    Map<String, int> set1={
+        'no':1,
+        'age':20
+    };
+    print(set1);
+}
